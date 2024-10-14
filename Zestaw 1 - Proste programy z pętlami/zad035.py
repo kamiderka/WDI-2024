1c01408a463bb0ffdf0dc3fd8ab7aeacf490a634
Kamil Kordecki <kamil@kordecki.dev>
2024-10-14 16:34:38 +0200

    test: Izad035

---
+++ Zestaw 1 - Proste programy z pętlami/zad035.py
@@ -0,0 +1,15 @@
+# Zadanie 35. Proszę napisać program wczytujący liczbę naturalną i odpowiadający na pytanie, czy liczba
+# ta zakończona jest unikalną cyfrą.
+
+def ends_with_unique_digit(n :int)->bool:
+    last_digit = n%10
+    n//=10
+    while n>0:
+        if n%10==last_digit:
+            return False
+        n//=10
+    return True
+
+
+n = int(input("Podaj liczbę: "))
+print("Czy kończy się unikalną cyfrą:", ends_with_unique_digit(n))
