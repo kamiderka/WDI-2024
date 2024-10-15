7e78b528e2c6a70a7e79d2e21fdc25ae6ab3ac2f
Kamil Kordecki <kamil@kordecki.dev>
2024-10-15 10:40:35 +0200

    solve: Izad035

--- Zestaw 1 - Proste programy z pętlami/zad035.py
+++ Zestaw 1 - Proste programy z pętlami/zad035.py
@@ -2,12 +2,15 @@
 # ta zakończona jest unikalną cyfrą.
 
 def ends_with_unique_digit(n :int)->bool:
+    assert n >=0
+    
     last_digit = n%10
     n//=10
     while n>0:
         if n%10==last_digit:
             return False
         n//=10
+    
     return True
 
 
