86e380b3cc336f3644d6d2eda28689983edeffb9
Kamil Kordecki <kamil@kordecki.dev>
2024-10-31 22:53:09 +0100

    solve: II zad 70

---
+++ Zestaw 2 - Tablice jednowymiarowe/zad070.py
@@ -0,0 +1,29 @@
+# Zadanie 70. Napisz funkcję, która dla N-elementowej tablicy T wypełnionej liczbami naturalnym 
+# wyznacza długość najdłuższego, spójnego podciągu geometrycznego
+
+
+# Przyda się obsługa obsługa dzielenia przez zero
+def len_of_longest_geo_progression(t :list)->int:
+    if len(t)<2:
+        return 0 
+
+    best_length = 2
+    counter = 2 
+    q = t[1]/t[0]
+    for i in range(2,len(t)):
+        if t[i]/t[i-1] == q:
+            counter += 1 
+        else:
+            best_length = max(best_length, counter)
+            counter = 2
+            q = t[i]/t[i-1]
+    return max(best_length,counter)
+
+assert len_of_longest_geo_progression([1,2,4,8,4,2, 1, 16, 16]) == 4
+assert len_of_longest_geo_progression([1,1]) == 2
+assert len_of_longest_geo_progression([0]) == 0
+assert len_of_longest_geo_progression([3,3,3,3,3,3]) == 6
+assert len_of_longest_geo_progression([1,3,9,81,729, 6561]) == 4
+assert len_of_longest_geo_progression([1,3,9,81,729, 6561, 4, 5]) == 4
+
+print("OK!")
